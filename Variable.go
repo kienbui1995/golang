cdaa0ee4255b3c54af01b26dd4ec6c3ae2c4a3dc
namjoker <nameffect@gmail.com>
2016-10-19 06:26:04 +0700

    Change Something

--- Variable.go
+++ Variable.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
+	var x string = "Hello World"
+	//Cach khoi tao ngan hon
+	y := " Hello, Y"
+	fmt.Println(x)
+	fmt.Println(y)
+
+	// Create Multiple Variable
+	var (
+		a = 1
+		b = 2
+		c = 4
+	)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
 }
